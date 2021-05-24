1601caf66208b67f8cc03a2daf33baaec41a7346
Franco Portin <fportin350@gmail.com>
2021-05-24 10:21:34 -0700

    changes to movie.py

--- app/models/movie.py
+++ app/models/movie.py
@@ -1,18 +1,18 @@
 from .db import db
 
-class Movie(db.model):
+class Movie(db.Model):
     __tablename__ = "movies"
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
-    image = db.Column(db.String, nullalbe=False)
+    image = db.Column(db.String, nullable=False)
     description = db.Column(db.Text, nullable=False)
     cast = db.Column(db.String, nullable=False)
     url = db.Column(db.String)
 
-    # bookmarks = db.relationship("Profile", secondary=bookmarks, lazy="subquery",
-    #     backref=db.backref('movies', lazy=True)
-    # )
+    bookmarks = db.relationship("Profile", secondary=bookmarks, lazy="subquery",
+        backref=db.backref('movies', lazy=True)
+    )
 
     profiles = db.relationship("Profile", secondary=bookmarks, back_populates="movies")
     genres = db.relationship("Genre", secondary=movieGenres, back_populates="movies" )
