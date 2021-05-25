706ba7a40ce612ef2c3877f1be9c3ea570ec6e21
miguel <miguelalvinflores@gmail.com>
2021-05-25 12:41:45 -0700

    changed favicon to circular, splash css

--- react-app/src/components/Splash.js
+++ react-app/src/components/Splash.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './CSS/Splash.css'
 
 const Splash = () => {
     // const dispatch = useDispatch();
 
     return (
         <div className="story-cards">
-            <div className="story-card">
+            <div className="story-card hero-card">
+                <div className="hero-card-background">
+                    <img className='hero-card-img' src='images/Splash-background.png' alt="Example Movie title panel" />
+                    <div class="concord-img-gradient"></div>
+                </div>
                 <div className="story-card-text">
                     <h1 className="story-card-title">
                         Unlimited Cat Videos, Cat Meows, and Purrs.
@@ -19,6 +24,7 @@ const Splash = () => {
                     <NavLink to='/signup'>Get Started</NavLink>
                     </div>
                 </div>
+
             </div>
         </div>
     );
