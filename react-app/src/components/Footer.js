53f6cb7e94b7a6748ede4e371fc5cd3128680e2a
alvin <huang.alvin.01@gmail.com>
2021-05-25 19:18:36 -0700

    working on footer, icons not showing

---
+++ react-app/src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./CSS/Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="bio-container">
+        <section>
+          <ul className="icons">
+            <li>
+              <a href="" class="icon brands fa-github alt"></a>Alvin Huang
+            </li>
+            <li>
+              <a href="" class="icon brands fa-github alt"></a>Miguel Flores
+            </li>
+            <li>
+              <a href="" class="icon brands fa-github alt"></a>Franco Portin
+            </li>
+          </ul>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Redirect, useLocation } from "react-router-dom";
 import { login } from "../../store/session";
-import "./LoginForm.css";
+import Footer from "../Footer.js";
+import "../CSS/LoginForm.css";
 
 const LoginForm = () => {
   const location = useLocation();
@@ -35,43 +36,64 @@ const LoginForm = () => {
   }
 
   return (
-    <div className="form-container">
-      <div className="main-form">
-        <h1 className="form-label">Sign In</h1>
-        <form onSubmit={onLogin}>
-          <div>
-            {errors.map((error) => (
-              <div>{error}</div>
-            ))}
-          </div>
-          <div>
-            {/* <label htmlFor="email">Email</label> */}
-            <input
-              name="email"
-              type="text"
-              placeholder="Email"
-              value={email}
-              onChange={updateEmail}
-              className="input-field"
-            />
-          </div>
-          <div>
-            {/* <label htmlFor="password">Password</label> */}
-            <input
-              name="password"
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={updatePassword}
-              className="input-field"
-            />
-            <div></div>
-            <button type="submit" className="sign-in-button">
-              Sign In
-            </button>
-          </div>
-        </form>
+    <div className="form-wrapper">
+      <img
+        className="hero-card-img"
+        src="images/Splash-background.png"
+        alt="Example Movie title panel"
+      />
+      <div className="form-container">
+        <div className="main-form">
+          <h1 className="form-label">Sign In</h1>
+          <form onSubmit={onLogin}>
+            <div>
+              {errors.map((error) => (
+                <div className="error">{error}</div>
+              ))}
+            </div>
+            <div id="float-label">
+              <label htmlFor="email">Email</label>
+              <input
+                name="email"
+                type="text"
+                // placeholder="Email"
+                value={email}
+                onChange={updateEmail}
+                className="input-field"
+              />
+            </div>
+            <div id="float-label">
+              <label htmlFor="password">Password</label>
+              <input
+                name="password"
+                type="password"
+                // placeholder="Password"
+                value={password}
+                onChange={updatePassword}
+                className="input-field"
+              />
+              <div></div>
+              <button type="submit" className="sign-in-button">
+                Sign In
+              </button>
+              <div className="form-other-details">
+                <p>
+                  New to Netflix?{" "}
+                  <a className="sign-up-link" href="/sign-up">
+                    {" "}
+                    Sign up now
+                  </a>{" "}
+                </p>
+                <p className="recaptcha-terms-of-use">
+                  This page is protected by Google reCAPTCHA to ensure you're
+                  not a bot. <span className="recaptcha-link">Learn more</span>.
+                </p>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
