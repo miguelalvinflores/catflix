66a502219fcd66e8a1ecff4bef2a16c480258a03
miguel <miguelalvinflores@gmail.com>
2021-05-27 10:27:19 -0700

    created movies model funciton and movies route

--- app/api/movie_routes.py
+++ app/api/movie_routes.py
@@ -3,13 +3,18 @@ from app.models import Profile, Movie, Like, db
 
 movie_routes = Blueprint('movie', __name__)
 
-@movie_routes.route('/<int:movieId>/likes/<int:profileId>', methods=['POST', 'DELETE'])
-def handleLikes(movieId, profileId):
-    if request.method == 'DELETE':
-        print('cat')
-    else:
-        upvoteDownvote = response.get_json()
-        like = Like(profileId=profileId, movieId=movieId, upvoteDownvote=upvoteDownvote)
-        db.session.add(like)
-        db.commit()
-        return
+
+# @movie_routes.route('/<int:movieId>/likes/<int:profileId>', methods=['POST', 'DELETE'])
+# def handleLikes(movieId, profileId):
+#     if request.method == 'DELETE':
+#         print('cat')
+#     else:
+#         upvoteDownvote = response.get_json()
+#         like = Like(profileId=profileId, movieId=movieId, upvoteDownvote=upvoteDownvote)
+#         db.session.add(like)
+#         db.commit()
+#         return
+@movie_routes.route('/<int:id>')
+def movies():
+    movies = Movie.query.all()
+    return {'movies': [movie.to_dict() for movie in movies]}
