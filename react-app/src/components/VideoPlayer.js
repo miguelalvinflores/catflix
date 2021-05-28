fa285f26b87990a923cbdcc2d1b951b4c8f834cb
alvin <huang.alvin.01@gmail.com>
2021-05-28 01:25:43 -0700

    video cover wip

---
+++ react-app/src/components/VideoPlayer.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { FiHelpCircle } from "react-icons/fi";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import { FaFastBackward, FaFastForward } from "react-icons/fa";
+import { FaPlay, FaPause } from "react-icons/fa";
+import { ImNext } from "react-icons/im";
+import { MdSubtitles } from "react-icons/md";
+import { RiFullscreenFill } from "react-icons/ri";
+import * as videoActions from "../store/video";
+import "./CSS/VideoPlayer.css";
+
+// const VideoPlayer = ({movieUrl}) => {
+// movieUrl destructured from props
+const VideoPlayer = () => {
+  const [videoIsActive, setVideoIsActive] = useState(true);
+  const [videoEnded, setVideoEnded] = useState(false);
+  const dispatch = useDispatch();
+
+  const togglePlay = () => {
+    const video = document.querySelector(".video");
+    if (video.paused || video.ended) {
+      setVideoIsActive(true);
+      video.play();
+    } else {
+      setVideoIsActive(false);
+      video.pause();
+    }
+  };
+  useEffect(() => {
+    const video = document.querySelector(".video");
+    video.addEventListener("timeupdate", () => {
+      let durationPos = video.currentTime / video.duration;
+      let duration = document.querySelector(".duration");
+      duration.style.width = durationPos * 100 + "%";
+      if (video.ended) {
+        dispatch(videoActions.setVideoEnd());
+      }
+    });
+  });
+  useEffect(() => {
+    console.log("hahaha the video is over");
+  }, [videoEnded]);
+  return (
+    <div className="c-video">
+      <video
+        className="video"
+        // change that url later
+        src="http://d23jaqdaucwmr3.cloudfront.net/1.mp4"
+        autoplay="true"
+        muted
+        // controls
+      ></video>
+      <div className="video-control-container">
+        <div className="duration-bar">
+          <div className="duration"></div>
+        </div>
+        <button className="control play" onClick={togglePlay}>
+          {videoIsActive ? <FaPause /> : <FaPlay />}
+        </button>
+        <button className="control rewind">
+          <FaFastBackward />
+        </button>
+        <button className=" control fastfoward">
+          <FaFastForward />
+        </button>
+        <button className=" control volume" id="volume">
+          <HiVolumeUp />
+        </button>
+        <input
+          class="volume-slider"
+          value="1"
+          type="range"
+          max="1"
+          min="0"
+          step="0.01"
+        ></input>
+        <span className="control title">movie title</span>
+        <button className=" control help">
+          <FiHelpCircle />
+        </button>
+        <button className=" control next">
+          <ImNext />
+        </button>
+        <button className=" control subtitle">
+          <MdSubtitles />
+        </button>
+        <button className=" control fullscreen">
+          <RiFullscreenFill />
+        </button>
+      </div>
+    </div>
+  );
+};
+export default VideoPlayer;
--- react-app/src/components/Watch.js
+++ react-app/src/components/Watch.js
@@ -1,22 +1,32 @@
 import React, { useState } from "react";
 import { Redirect, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
-
-// grab movieId from useParams make api call to back end for movie Object
-// pass movie.url into video component as prop
+import VideoPlayer from "./VideoPlayer";
+import VideoCover from "./VideoCover";
+import Footer from "./Footer";
+import "./CSS/Watch.css";
 
 const Watch = () => {
+  const [showMovieCover, setShowMovieCover] = useState(true);
   const { movieId } = useParams();
-  const movieObj = fetch(`/api/movies/${movieId}`).then((res) =>
-    console.log(res)
-  );
-  // console.log(movieObj);
+  // const movie = useSelector(state=>state.movies[movieId])
+  const videoEnded = useSelector((state) => state.video.end);
 
+  // define movieCover here. useEffect listens to play button on movieCover
+  // clicking play button renders VideoPlayer instead w/ autoplay
+  // movieCover playbutton onClick sets showMovieCover(false) && videoEnded(false)
   return (
     <>
-      <div className="video-container">{/* video component */}</div>
+      <div className="video-container">
+        {/* pass in video url */}
+        {/* <VideoCover movie={movie} /> */}
+        {/* <VideoPlayer movieUrl={ movie.url} /> */}
+        {showMovieCover || videoEnded ? <VideoCover /> : <VideoPlayer />}
+        {/* <VideoPlayer /> */}
+      </div>
       {/* carousel: top picks for you */}
       {/* detail component */}
+      {/* <Footer /> */}
     </>
   );
 };
